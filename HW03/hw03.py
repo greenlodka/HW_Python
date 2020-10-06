0fd606fb9a93a5c7f261cb735dbf4e60411ac5f4
green_submarine@mail.ru <green_submarine@mail.ru>
2020-10-06 23:59:08 +0300

    hw03.py

---
+++ HW03/hw03.py
@@ -0,0 +1,29 @@
+##Задание 1
+##s = str(input("Введите слово: "))
+##for symbol in s:
+##    if int(symbol) % 2 == 0:
+##        continue
+##    if symbol == "а" or symbol == "к":
+##        break
+##    print(symbol)
+
+
+##Задание 2
+##s = str(input("Введите слово: "))
+##a = s[::-1]
+##print(a)
+##for symbol in a:
+##    if symbol != "з" and symbol != "я":
+##        break
+##        print(a)
+
+
+
+##Задание 3
+##a = int(input("Введите число: "))
+##s = str
+##for s in range(a):
+##    s = input("Введите слово: ")
+##
+##    if s == "программирование":
+##        break
